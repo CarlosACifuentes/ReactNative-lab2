9c72bff881bb15fc2d97da976a4ca10303dc3348
Carlos Cifuentes <cabetocifuentes@gmail.com>
2021-09-24 15:11:28 -0400

    fixed padding

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create (
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      padding:150,
+      padding:100,
     },
     edit:{
       borderColor: 'black',
